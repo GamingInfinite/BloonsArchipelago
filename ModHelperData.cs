596462c66bff5cedc66f27207fe8e91678ffbf1b
VoidBaroness <52724070+GamingInfinite@users.noreply.github.com>
2023-12-28 18:18:57 -0600

    Bump Version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace BloonsArchipelago;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "40.0";
-    public const string Version = "0.0.1";
+    public const string Version = "0.0.2";
     public const string Name = "Bloons Archipelago";
 
     public const string Description = "A mod that connects Bloons to the Archipelago Multiworld Randomizer.";
