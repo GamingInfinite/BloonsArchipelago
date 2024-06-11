732d988da0698e7411843f37ff92a395e6ae85f2
VoidBaroness <52724070+GamingInfinite@users.noreply.github.com>
2024-06-11 16:00:28 -0500

    erm all mode unlocks
    one would think this wasn't required

--- LATEST.md
+++ LATEST.md
@@ -1,5 +1,4 @@
-# Bloons AP v(0.3.3/0.2.1) (Mod/APWorld)
-- Golden Bloons no Longer Break Maps
-- MEDAL TRACKER
+# Bloons AP v(0.3.4/0.2.1) (Mod/APWorld)
+- I'm definitely smart i promise
 
 For setup information please check the Readme
---
+++ Patches/MapMenu/AllModesUnlocked.cs
@@ -0,0 +1,55 @@
+﻿using BTD_Mod_Helper;
+using HarmonyLib;
+using Il2CppAssets.Scripts.Unity.UI_New.Main.ModeSelect;
+using System.Collections.Generic;
+
+namespace BloonsArchipelago.Patches.MapMenu
+{
+    [HarmonyPatch(typeof(ModeButton), nameof(ModeButton.Update))]
+    internal class AllModesUnlocked
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ModeButton __instance)
+        {
+            if (BloonsArchipelago.sessionHandler.ready)
+            {
+                List<string> unlockedModes = new()
+                {
+                    "Standard"
+                };
+                if (BloonsArchipelago.sessionHandler.Difficulty >= 4)
+                {
+                    unlockedModes.Add("Impoppable");
+                }
+                if (BloonsArchipelago.sessionHandler.Difficulty >= 5)
+                {
+                    unlockedModes.Add("Clicks");
+                }
+                if (BloonsArchipelago.sessionHandler.Difficulty > 5)
+                {
+                    unlockedModes.Add(__instance.name);
+                }
+
+                if (unlockedModes.Contains(__instance.name))
+                {
+                    if (__instance.currentState != "Unlock")
+                    {
+                        __instance.Unlock();
+                        __instance.DisplayUnlockAnimation();
+                        __instance.currentState = "Unlock";
+                    }
+                } else
+                {
+                    if (__instance.currentState != "Lock")
+                    {
+                        __instance.Lock();
+                        __instance.currentState = "Lock";
+                    }
+                }
+
+                return false;
+            }
+            return true;
+        }
+    }
+}
